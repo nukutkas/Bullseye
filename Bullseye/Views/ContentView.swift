8ae75ae7c5c3294d45330ff7c72f82bb798ace5d
Nikita Kochetov <nothsmple@mail.ru>
2021-10-21 21:16:39 +0300

    Type Inference

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var alertIsVisible: Bool = false
-    @State private var sliderValue: Double = 50.0
-    @State private var game: Game = Game()
-    
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
     
     var body: some View {
         VStack {
@@ -33,19 +32,19 @@ struct ContentView: View {
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: self.$sliderValue, in: 1.0...100.0)
+                Slider(value: $sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
             }
             Button(action: {
                 print("Hello, SwiftUI")
-                self.alertIsVisible = true
+                alertIsVisible = true
             }) {
                 Text("Hit me")
             }
             .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue: Int = Int(self.sliderValue.rounded())
-                return Alert(title: Text("Hello, there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
+                let roundedValue = Int(sliderValue.rounded())
+                return Alert(title: Text("Hello, there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \( game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
             })
         }
     }
