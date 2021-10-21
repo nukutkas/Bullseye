efb15f7034b0a4f04edb747b7a7ec9231b8fdc9e
Nikita Kochetov <nothsmple@mail.ru>
2021-10-21 15:18:06 +0300

    Files have been regrouped and Game model has been created.

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Татьяна Кочетова on 21.10.2021.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score: Int = 0
+    var round: Int = 1
+}
