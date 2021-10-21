d7d4d6186f9b3255b963ed35e6679366ec73b47b
Nikita Kochetov <nothsmple@mail.ru>
2021-10-21 11:55:13 +0300

    sliderValue has been added.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var alertIsVisible: Bool = false
-    @State private var whosThereIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    
     
     var body: some View {
         VStack {
@@ -31,7 +32,7 @@ struct ContentView: View {
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
             }
@@ -43,14 +44,6 @@ struct ContentView: View {
             }
             .alert(isPresented: $alertIsVisible, content: {
                 return Alert(title: Text("Hello, there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
-            })
-            Button(action: {
-                self.whosThereIsVisible = true
-            }) {
-                Text("Knock Knock")
-            }
-            .alert(isPresented: $whosThereIsVisible, content: {
-                return Alert(title: Text("Who's There?"), message: Text("Little old lady."), dismissButton: .default(Text("Little old lady who?")))
             })
         }
     }
