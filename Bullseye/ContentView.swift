e9dab89d911ce4d018a54bbeed6a5bc66c178b9f
Nikita Kochetov <nothsmple@mail.ru>
2021-10-21 14:55:49 +0300

    Made sliderValue rounded.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -43,7 +43,8 @@ struct ContentView: View {
                 Text("Hit me")
             }
             .alert(isPresented: $alertIsVisible, content: {
-                return Alert(title: Text("Hello, there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                let roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello, there!"), message: Text("The slider's value is \(roundedValue)."), dismissButton: .default(Text("Awesome!")))
             })
         }
     }
