566dfa2744a4c78dd077235f3a21236c90234ba8
Nikita Kochetov <nothsmple@mail.ru>
2021-11-04 11:05:08 +0300

    ZStack has been added,

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,43 +14,47 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-                .padding(.leading, 30.0)
-                .padding(.trailing, 30.0)
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            HStack {
-                Text("1")
+        ZStack {
+            Color(red: 243.0 / 255.0, green: 248.0 / 255.0, blue: 253.0 / 255.0)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
                     .bold()
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .padding(.leading, 30.0)
+                    .padding(.trailing, 30.0)
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    Text("100")
+                        .bold()
+                }
+                .padding()
+                Button(action: {
+                    print("Hello, SwiftUI!")
+                    alertIsVisible = true
+                }) {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding(20.0)
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .cornerRadius(21.0)
+                .alert(isPresented: $alertIsVisible, content: {
+                    let roundedValue = Int(sliderValue.rounded())
+                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+                })
             }
-            .padding()
-            Button(action: {
-                print("Hello, SwiftUI!")
-                alertIsVisible = true
-            }) {
-                Text("Hit me".uppercased())
-                    .bold()
-                    .font(.title3)
-            }
-            .padding(20.0)
-            .background(Color.blue)
-            .foregroundColor(Color.white)
-            .cornerRadius(21.0)
-            .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue = Int(sliderValue.rounded())
-                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
-            })
         }
     }
 }
