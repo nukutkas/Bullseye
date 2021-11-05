293efeb994779102682396c5fa417e9a05cc33e1
Nikita Kochetov <nothsmple@mail.ru>
2021-11-05 16:05:29 +0300

    Background View has been added.

---
+++ Bullseye/Views/BackgroundView.swift
@@ -0,0 +1,41 @@
+//
+//  BackgroundView.swift
+//  Bullseye
+//
+//  Created by Татьяна Кочетова on 05.11.2021.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack {
+            TopView(game: $game)
+        }
+        .padding()
+        .background(
+            Color("Background Color")
+                .edgesIgnoringSafeArea(.all)
+        )
+    }
+}
+
+struct TopView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            Spacer()
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView(game: .constant(Game()))
+    }
+}
