c145ce6bec70fde4e2eb6e1e4d4bb08065aed366
Nikita Kochetov <nothsmple@mail.ru>
2021-11-05 14:39:30 +0300

    RoundedImageViewStroked and filled have been added.

---
+++ Bullseye/Views/RoundViews.swift
@@ -0,0 +1,48 @@
+//
+//  RoundViews.swift
+//  Bullseye
+//
+//  Created by Татьяна Кочетова on 05.11.2021.
+//
+
+import SwiftUI
+
+struct RoundedImageViewStroked: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56.0, height: 56.0)
+            .overlay(Circle().stroke())
+    }
+}
+
+struct RoundedImageViewFilled: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56.0, height: 56.0)
+            .background(Circle().fill())
+    }
+}
+struct PreviewView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
+    }
+}
+
+struct RoundViews_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewView()
+        PreviewView()
+            .preferredColorScheme(.dark)
+    }
+}
