05b92f7aa587d83493ff7f8d464a01ec07fdf983
Nikita Kochetov <nothsmple@mail.ru>
2021-11-05 16:13:25 +0300

    BottomView has been added

--- Bullseye/Views/BackgroundView.swift
+++ Bullseye/Views/BackgroundView.swift
@@ -34,6 +34,28 @@ struct TopView: View {
     }
 }
 
+struct NumberView: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        Color.gray
+            .frame(width: 56.0, height: 56.0)
+    }
+}
+
+struct BottomView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            NumberView(title: "Score", text: String(game.score))
+            Spacer()
+            NumberView(title: "Round", text: String(game.round))
+        }
+    }
+}
+
 struct BackgroundView_Previews: PreviewProvider {
     static var previews: some View {
         BackgroundView(game: .constant(Game()))
