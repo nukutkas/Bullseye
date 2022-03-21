9ca5332c81b54ed55fed5ba3ae76f06c06933d38
Nikita Kochetov <nothsmple@mail.ru>
2022-03-21 22:49:28 +0300

    func addToLeaderboard has been added.

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -58,6 +58,16 @@ class BullseyeTests: XCTestCase {
         game.restart()
         XCTAssertEqual(game.score, 0)
         XCTAssertEqual(game.round, 1)
+    }
+    
+    func testLeaderBoard() {
+        game.startNewRound(points: 100)
+        XCTAssertEqual(game.leaderboardEntries.count, 1)
+        XCTAssertEqual(game.leaderboardEntries[0].score, 100)
+        game.startNewRound(points: 200)
+        XCTAssertEqual(game.leaderboardEntries.count, 2)
+        XCTAssertEqual(game.leaderboardEntries[0].score, 200)
+        XCTAssertEqual(game.leaderboardEntries[1].score, 100)
         
     }
 }
