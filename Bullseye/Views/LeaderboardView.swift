23c180b52901c5cd86c68198484cec0962110cd9
Nikita Kochetov <nothsmple@mail.ru>
2022-03-21 09:54:03 +0300

    LeaderboardRowColor has been added.

---
+++ Bullseye/Views/LeaderboardView.swift
@@ -0,0 +1,43 @@
+//
+//  LeaderboardView.swift
+//  Bullseye
+//
+//  Created by Татьяна Кочетова on 21.03.2022.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        RowView(index: 1, score: 10, date: Date())    }
+}
+
+struct RowView: View {
+    let index:Int
+    let score: Int
+    let date: Date
+    
+    var body: some View {
+        HStack {
+            Slider(value: .constant(50.0))
+                .padding()
+        }
+    }
+}
+
+struct LeaderboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LeaderboardView()
+            LeaderboardView()
+                .previewLayout(.fixed(width: 568, height: 320
+                                     ))
+            LeaderboardView()
+                .preferredColorScheme(.dark)
+            LeaderboardView()
+                .preferredColorScheme(.dark)
+                .previewLayout(.fixed(width: 568, height: 320
+                                     ))
+        }
+    }
+}
